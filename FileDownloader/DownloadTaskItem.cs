dce201efa530f8e776ae8e3f992547147ada7aaf
DavidHawkes <db300@163.com>
2024-04-01 00:26:55 +0800

    下载器支持导入excel任务

---
+++ FileDownloader/DownloadTaskItem.cs
@@ -0,0 +1,17 @@
+﻿namespace FileDownloader
+{
+    /// <summary>
+    /// 下载任务实体
+    /// </summary>
+    internal class DownloadTaskItem
+    {
+        /// <summary>
+        /// 下载链接
+        /// </summary>
+        public string Url { get; set; }
+        /// <summary>
+        /// 保存文件名
+        /// </summary>
+        public string FileName { get; set; }
+    }
+}
--- FileDownloader/MainForm.cs
+++ FileDownloader/MainForm.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using ExcelDataReader;
+using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Security.Policy;
 using System.Windows.Forms;
 
 namespace FileDownloader
@@ -26,13 +29,13 @@ namespace FileDownloader
         private static string DownloadDir => Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Downloads");
         private TextBox _txtTask;
         private TextBox _txtLog;
+        private static readonly List<DownloadTaskItem> _downloadTaskList = new List<DownloadTaskItem>();
         #endregion
 
         #region event handler
         private void BtnDownload_Click(object sender, EventArgs e)
         {
-            var urlList = _txtTask.Lines.ToList();
-            var totalCount = urlList?.Count;
+            var totalCount = _downloadTaskList?.Count;
             if (!(totalCount > 0)) return;
             var dir = DownloadDir;
             if (!Directory.Exists(dir)) Directory.CreateDirectory(dir);
@@ -42,12 +45,12 @@ namespace FileDownloader
                 var webClient = new WebClient();
                 for (var i = 0; i < totalCount; i++)
                 {
-                    var url = urlList[i];
+                    var url = _downloadTaskList[i].Url;
+                    var fileName = _downloadTaskList[i].FileName;
                     try
                     {
                         var tmpFileName = Path.GetTempFileName();
                         webClient.DownloadFile(url, tmpFileName);
-                        var fileName = Path.GetFileName(Uri.UnescapeDataString(url));
                         File.Move(tmpFileName, Path.Combine(dir, fileName));
                         background.ReportProgress(i + 1, $"{url} 下载完成");
                     }
@@ -66,6 +69,53 @@ namespace FileDownloader
                 _txtLog.AppendText("下载完成\r\n");
             };
             background.RunWorkerAsync();
+        }
+
+        private void BtnImportExcel_Click(object sender, EventArgs e)
+        {
+            var openDlg = new OpenFileDialog();
+            if (openDlg.ShowDialog() != DialogResult.OK) return;
+            _downloadTaskList.Clear();
+            using (var fs = File.OpenRead(openDlg.FileName))
+            {
+                using (var reader = ExcelReaderFactory.CreateReader(fs))
+                {
+                    do
+                    {
+                        while (reader.Read())
+                        {
+                            var fieldCount = reader.FieldCount;
+                            switch (fieldCount)
+                            {
+                                case 1:
+                                    {
+                                        var url = reader.GetString(0).Trim();
+                                        _downloadTaskList.Add(new DownloadTaskItem
+                                        {
+                                            Url = url,
+                                            FileName = Path.GetFileName(Uri.UnescapeDataString(url))
+                                        });
+                                    }
+                                    break;
+                                case 2:
+                                    {
+                                        var url = reader.GetString(0).Trim();
+                                        var fileName = reader.GetString(1).Trim();
+                                        _downloadTaskList.Add(new DownloadTaskItem
+                                        {
+                                            Url = url,
+                                            FileName = fileName
+                                        });
+                                    }
+                                    break;
+                                default:
+                                    break;
+                            }
+                        }
+                    } while (reader.NextResult());
+                }
+            }
+            _txtTask.Text = string.Join(Environment.NewLine, _downloadTaskList.Select(x => x.Url));
         }
         #endregion
 
@@ -91,16 +141,27 @@ namespace FileDownloader
                 ForeColor = Color.Red,
                 Location = new Point(ControlMargin, ControlMargin),
                 Parent = this,
-                Text = "将文件链接粘贴到下方任务列表（一条链接一行）"
+                Text = "将文件链接粘贴到下方任务列表（一条链接一行）",
+                Visible = false
             };
+
+            var btnImportExcel = new Button
+            {
+                AutoSize = true,
+                Location = new Point(ControlMargin, ControlMargin),
+                Parent = this,
+                Text = "导入Excel"
+            };
+            btnImportExcel.Click += BtnImportExcel_Click;
 
             _txtTask = new TextBox
             {
                 Anchor = AnchorStyles.Left | AnchorStyles.Top | AnchorStyles.Right,
-                Location = new Point(ControlMargin, lbl.Bottom + ControlPadding),
+                Location = new Point(ControlMargin, btnImportExcel.Bottom + ControlPadding),
                 MaxLength = 0,
                 Multiline = true,
                 Parent = this,
+                ReadOnly = true,
                 ScrollBars = ScrollBars.Both,
                 Size = new Size(ClientSize.Width - 2 * ControlMargin, 200),
                 WordWrap = false
