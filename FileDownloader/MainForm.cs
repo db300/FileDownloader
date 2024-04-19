6fa322187aeb4edcd96426ede92f3b8b563558db
DavidHawkes <db300@163.com>
2024-04-19 15:32:14 +0800

    更新版本

--- FileDownloader/MainForm.cs
+++ FileDownloader/MainForm.cs
@@ -6,7 +6,6 @@ using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Net;
-using System.Security.Policy;
 using System.Windows.Forms;
 
 namespace FileDownloader
@@ -24,6 +23,7 @@ namespace FileDownloader
 
         #region property
         private const string Url4Appreciate = "https://www.yuque.com/docs/share/4d2ad434-a4fe-40a1-b530-c61811d5226e?# 《打赏说明》";
+        private const string Url4Readme = "https://www.yuque.com/lengda/eq8cm6/uwah0b1xer1d2rdt";
         private const int ControlMargin = 20;
         private const int ControlPadding = 12;
         private static string DownloadDir => Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Downloads");
@@ -117,6 +117,35 @@ namespace FileDownloader
             }
             _txtTask.Text = string.Join(Environment.NewLine, _downloadTaskList.Select(x => x.Url));
         }
+
+        private void BtnImportTxt_Click(object sender, EventArgs e)
+        {
+            var openDlg = new OpenFileDialog();
+            if (openDlg.ShowDialog() != DialogResult.OK) return;
+            _downloadTaskList.Clear();
+            var s = File.ReadAllText(openDlg.FileName);
+            var urls = s.Split('\n').Select(x => x.Trim()).Where(x => !string.IsNullOrEmpty(x)).ToList();
+            _downloadTaskList.AddRange(urls.Select(x => new DownloadTaskItem
+            {
+                Url = x,
+                FileName = Path.GetFileName(Uri.UnescapeDataString(x))
+            }));
+            _txtTask.Text = string.Join(Environment.NewLine, _downloadTaskList.Select(x => x.Url));
+        }
+
+        private void BtnPaste_Click(object sender, EventArgs e)
+        {
+            var s = Clipboard.GetText();
+            if (string.IsNullOrEmpty(s)) return;
+            _downloadTaskList.Clear();
+            var urls = s.Split('\n').Select(x => x.Trim()).Where(x => !string.IsNullOrEmpty(x)).ToList();
+            _downloadTaskList.AddRange(urls.Select(x => new DownloadTaskItem
+            {
+                Url = x,
+                FileName = Path.GetFileName(Uri.UnescapeDataString(x))
+            }));
+            _txtTask.Text = string.Join(Environment.NewLine, _downloadTaskList.Select(x => x.Url));
+        }
         #endregion
 
         #region ui
@@ -124,26 +153,6 @@ namespace FileDownloader
         {
             StartPosition = FormStartPosition.CenterScreen;
             Text = $"文件下载器 {System.Reflection.Assembly.GetExecutingAssembly().GetName().Version}";
-
-            var lkbl = new LinkLabel
-            {
-                Anchor = AnchorStyles.Top | AnchorStyles.Right,
-                AutoSize = true,
-                Parent = this,
-                Text = "如果觉得好用，来打赏一下啊 O(∩_∩)O 哈哈~"
-            };
-            lkbl.Location = new Point(ClientSize.Width - ControlMargin - lkbl.Width, ControlMargin);
-            lkbl.LinkClicked += (sender, e) => { System.Diagnostics.Process.Start(Url4Appreciate); };
-
-            var lbl = new Label
-            {
-                AutoSize = true,
-                ForeColor = Color.Red,
-                Location = new Point(ControlMargin, ControlMargin),
-                Parent = this,
-                Text = "将文件链接粘贴到下方任务列表（一条链接一行）",
-                Visible = false
-            };
 
             var btnImportExcel = new Button
             {
@@ -153,6 +162,24 @@ namespace FileDownloader
                 Text = "导入Excel"
             };
             btnImportExcel.Click += BtnImportExcel_Click;
+
+            var btnImportTxt = new Button
+            {
+                AutoSize = true,
+                Location = new Point(btnImportExcel.Right + ControlPadding, btnImportExcel.Top),
+                Parent = this,
+                Text = "导入文本"
+            };
+            btnImportTxt.Click += BtnImportTxt_Click;
+
+            var btnPaste = new Button
+            {
+                AutoSize = true,
+                Location = new Point(btnImportTxt.Right + ControlPadding, btnImportExcel.Top),
+                Parent = this,
+                Text = "直接粘贴"
+            };
+            btnPaste.Click += BtnPaste_Click;
 
             _txtTask = new TextBox
             {
@@ -179,7 +206,7 @@ namespace FileDownloader
             };
             btnDownload.Click += BtnDownload_Click;
 
-            lbl = new Label
+            var lbl = new Label
             {
                 AutoSize = true,
                 Location = new Point(btnDownload.Right + ControlPadding, btnDownload.Top + 6),
@@ -198,6 +225,25 @@ namespace FileDownloader
                 Size = new Size(ClientSize.Width - 2 * ControlMargin, ClientSize.Height - ControlMargin - ControlPadding - btnDownload.Bottom),
                 WordWrap = false
             };
+
+            var lkbl = new LinkLabel
+            {
+                Anchor = AnchorStyles.Top | AnchorStyles.Right,
+                AutoSize = true,
+                Parent = this,
+                Text = "如果觉得好用，来打赏一下啊~"
+            };
+            lkbl.Location = new Point(ClientSize.Width - ControlMargin - lkbl.Width, btnImportExcel.Top + (btnImportExcel.Height - lkbl.Height) / 2);
+            lkbl.LinkClicked += (sender, e) => { System.Diagnostics.Process.Start(Url4Appreciate); };
+
+            lkbl = new LinkLabel
+            {
+                AutoSize = true,
+                Parent = this,
+                Text = "点击查看使用说明"
+            };
+            lkbl.Location = new Point(btnPaste.Right + ControlPadding, btnImportExcel.Top + (btnImportExcel.Height - lkbl.Height) / 2);
+            lkbl.LinkClicked += (sender, e) => { System.Diagnostics.Process.Start(Url4Readme); };
         }
         #endregion
     }
